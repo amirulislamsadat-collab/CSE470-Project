14cd27467a4958c6811297a97b348484d19bec53
MD. AMIRUL ISLAM SADAT <amirul.islam.sadat@g.bracu.ac.bd>
2026-09-02 02:05:19 +0600

    Revert "Ramisa faria puspita"

--- controllers/setupController.js
+++ controllers/setupController.js
@@ -6,9 +6,9 @@ const Module = require('../models/Module');
 const User   = require('../models/User');
 
 const roleRecommendations = {
-  'Student':      [1, 2, 4, 6, 12, 13],
-  'Professional': [1, 3, 5, 13],
-  'Freelancer':   [1, 3, 5, 6, 12, 13]
+  'Student':      [1, 2, 4, 6],
+  'Professional': [1, 3, 5],
+  'Freelancer':   [1, 3, 5, 6]
 };
 
 exports.getSetup = async (req, res) => {
